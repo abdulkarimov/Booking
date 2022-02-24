d22d738242f8e2646808f8fe65e7488d3af47846
ITstart <itstart@itstep.loc>
2022-02-24 21:43:50 +0600

    ss

--- Booking/app/Services/BaseService.php
+++ Booking/app/Services/BaseService.php
@@ -1,26 +1,8 @@
 <?php
+namespace App\Services;
+
 abstract class BaseService
 {
-    public $repo;
-
-    public function all()
-    {
-        return $this->repo->all();
-    }
-
-    public function create(array $input)
-    {
-        return $this->repo->create($input);
-    }
-
-    public function update($id, array $input)
-    {
-        return $this->repo->update($id, $input);
-    }
-    public function destroy($id)
-    {
-        return $this->repo->destroy($id);
-    }
 
 
 }
