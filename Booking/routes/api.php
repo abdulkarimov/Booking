dae71797e4d9352a0a8e6b99022ac5c073712256
HeniTay <79849423+abdulkarimov@users.noreply.github.com>
2022-03-04 09:37:26 +0600

    Update api.php

--- Booking/routes/api.php
+++ Booking/routes/api.php
@@ -18,6 +18,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::group(['prefix' => '/city'], function () {
     Route::get('/', [CityController::class, 'getAll']);
+    Route::get('/{id}', [CityController::class, 'getById']);
     Route::post('/', [CityController::class, 'add']);
     Route::put('/{id}', [CityController::class, 'edit']);
     Route::delete('/{id}', [CityController::class, 'delete']);
@@ -25,21 +26,15 @@ Route::group(['prefix' => '/city'], function () {
 
 Route::group(['prefix' => '/country'], function () {
     Route::get('/', [CountryController::class, 'getAll']);
+    Route::get('/{id}', [CountryController::class, 'getById']);
     Route::post('/', [CountryController::class, 'add']);
     Route::put('/{id}', [CountryController::class, 'edit']);
     Route::delete('/{id}', [CountryController::class, 'delete']);
-});
-
-
-Route::group(['prefix' => '/cabinet'], function () {
-    Route::get('/', [CabinetController::class, 'getAll']);
-    Route::post('/', [CabinetController::class, 'add']);
-    Route::put('/{id}', [CabinetController::class, 'edit']);
-    Route::delete('/{id}', [CabinetController::class, 'delete']);
-});
+    });
 
 Route::group(['prefix' => '/booking'], function () {
     Route::get('/', [BookingController::class, 'getAll']);
+    Route::get('/{id}', [BookingController::class, 'getById']);
     Route::post('/', [BookingController::class, 'add']);
     Route::put('/{id}', [BookingController::class, 'edit']);
     Route::delete('/{id}', [BookingController::class, 'delete']);
@@ -47,8 +42,8 @@ Route::group(['prefix' => '/booking'], function () {
 
 Route::group(['prefix' => '/building'], function () {
     Route::get('/', [BuildingController::class, 'getAll']);
+    Route::get('/{id}', [BuildingController::class, 'getById']);
     Route::post('/', [BuildingController::class, 'add']);
     Route::put('/{id}', [BuildingController::class, 'edit']);
     Route::delete('/{id}', [BuildingController::class, 'delete']);
 });
-
